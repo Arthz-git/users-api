28d9f5545651a9aa24380f931b36f0d449ed5ac9
Arthur <arthurcamara48@gmail.com>
2023-04-03 21:46:57 -0300

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const HomeController = require('../controllers/HomeController')
+const UserController = require('../controllers/UserController')
+const AdminAuth = require('../middleware/AdminAuth')
+
+const router = express.Router()
+
+router.get('/', HomeController.index)
+router.post('/user', UserController.create)
+router.get('/user', AdminAuth, UserController.index)
+router.get('/user/:id', AdminAuth, UserController.findUser)
+router.put('/user', AdminAuth, UserController.edit)
+router.delete('/user/:id', AdminAuth, UserController.remove)
+
+router.post('/recoverpassword', UserController.recoverPassword)
+router.post('/changepassword', UserController.changePassword)
+
+router.post('/login', UserController.login)
+
+module.exports = router
